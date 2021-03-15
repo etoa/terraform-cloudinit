cda0b7d2355d1ab431b9d5ec9cc99c176f1c20c3
Nicolas Perrenoud <mail@nicu.ch>
2021-03-15 18:05:24 +0200

    Added test round

--- main.tf
+++ main.tf
@@ -11,40 +11,11 @@ variable "hcloud_token" {}
 
 locals {
   ssh_keys = ["mrcage"]
+  location = "nbg1"
 }
 
 provider "hcloud" {
   token = var.hcloud_token
-}
-
-resource "hcloud_server" "round20" {
-  name = "round20.etoa.net"
-  image = "ubuntu-18.04"
-  server_type = "cx11"
-  location = "nbg1"
-  ssh_keys = local.ssh_keys
-  user_data = templatefile("cloud_config.yaml", {
-    hostname = "round20.etoa.net"
-    public_ip = "78.47.232.230"
-    webapp_branch = "3.6-stable"
-    data_disk = hcloud_volume.round20_data.linux_device
-  })
-  firewall_ids = [hcloud_firewall.web_fw.id]
-  depends_on = [
-    hcloud_volume.round20_data,
-  ]
-}
-
-resource "hcloud_volume" "round20_data" {
-  name = "round20-data"
-  size = 10
-  location = "nbg1"
-  format = "ext4"
-}
-
-resource "hcloud_volume_attachment" "round20_data" {
-  volume_id = hcloud_volume.round20_data.id
-  server_id = hcloud_server.round20.id
 }
 
 resource "hcloud_firewall" "web_fw" {
@@ -84,4 +55,64 @@ resource "hcloud_firewall" "web_fw" {
       "::/0"
    ]
   }
+}
+
+resource "hcloud_server" "round20" {
+  name = "round20.etoa.net"
+  image = "ubuntu-18.04"
+  server_type = "cx11"
+  location = local.location
+  ssh_keys = local.ssh_keys
+  user_data = templatefile("cloud_config.yaml", {
+    hostname = "round20.etoa.net"
+    public_ip = "78.47.232.230"
+    webapp_branch = "3.6-stable"
+    data_disk = hcloud_volume.round20_data.linux_device
+  })
+  firewall_ids = [hcloud_firewall.web_fw.id]
+  depends_on = [
+    hcloud_volume.round20_data,
+  ]
+}
+
+resource "hcloud_volume" "round20_data" {
+  name = "round20-data"
+  size = 10
+  location = local.location
+  format = "ext4"
+}
+
+resource "hcloud_volume_attachment" "round20_data" {
+  volume_id = hcloud_volume.round20_data.id
+  server_id = hcloud_server.round20.id
+}
+
+resource "hcloud_server" "test" {
+  name = "test.etoa.net"
+  image = "ubuntu-18.04"
+  server_type = "cx11"
+  location = local.location
+  ssh_keys = local.ssh_keys
+  user_data = templatefile("cloud_config.yaml", {
+    hostname = "test.etoa.net"
+    public_ip = "116.202.178.35"
+    webapp_branch = "master"
+    data_disk = hcloud_volume.test_data.linux_device
+  })
+  firewall_ids = [hcloud_firewall.web_fw.id]
+  depends_on = [
+    hcloud_volume.test_data,
+  ]
+}
+
+resource "hcloud_volume" "test_data" {
+  name = "test-data"
+  size = 10
+  location = local.location
+  format = "ext4"
+}
+
+resource "hcloud_volume_attachment" "test_data" {
+  volume_id = hcloud_volume.test_data.id
+  server_id = hcloud_server.test.id
 }
