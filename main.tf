1b1d064f6741320e694732aedc361b7b8cc2e14f
Nicolas Perrenoud <mail@nicu.ch>
2021-03-15 17:22:05 +0200

    Storage disk

--- main.tf
+++ main.tf
@@ -17,18 +17,34 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
-resource "hcloud_server" "test01" {
-  name = "test01.etoa.net"
+resource "hcloud_server" "round20" {
+  name = "round20.etoa.net"
   image = "ubuntu-18.04"
   server_type = "cx11"
   location = "nbg1"
   ssh_keys = local.ssh_keys
   user_data = templatefile("cloud_config.yaml", {
-    hostname = "test01.etoa.net"
+    hostname = "round20.etoa.net"
     public_ip = "78.47.232.230"
-    webapp_branch = "master"
+    webapp_branch = "3.6-stable"
+    data_disk = hcloud_volume.round20_data.linux_device
   })
   firewall_ids = [hcloud_firewall.web_fw.id]
+  depends_on = [
+    hcloud_volume.round20_data,
+  ]
+}
+
+resource "hcloud_volume" "round20_data" {
+  name = "round20-data"
+  size = 10
+  location = "nbg1"
+  format = "ext4"
+}
+
+resource "hcloud_volume_attachment" "round20_data" {
+  volume_id = hcloud_volume.round20_data.id
+  server_id = hcloud_server.round20.id
 }
 
 resource "hcloud_firewall" "web_fw" {
